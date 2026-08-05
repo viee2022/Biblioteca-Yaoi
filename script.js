ffe1c146443f5a6f1088aee6630fa0c13807f82b
viee2022 <vitoriabeck0713@gmail.com>
2026-08-05 18:43:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+const biblioteca = [];
+
+const lista = document.getElementById("biblioteca");
+
+function atualizarBiblioteca(){
+
+lista.innerHTML="";
+
+biblioteca.forEach((manga)=>{
+
+lista.innerHTML += `
+
+<div class="manga">
+
+<img src="${manga.capa}">
+
+<h3>${manga.nome}</h3>
+
+<p>Capítulo ${manga.atual}/${manga.total}</p>
+
+<button onclick="window.open('${manga.link}')">
+
+Ler Agora
+
+</button>
+
+</div>
+
+`;
+
+});
+
+}
+
+biblioteca.push({
+
+nome:"Love is an Illusion",
+
+atual:87,
+
+total:120,
+
+link:"https://google.com",
+
+capa:"https://placehold.co/300x420"
+
+});
+
+biblioteca.push({
+
+nome:"Jinx",
+
+atual:73,
+
+total:"...",
+
+link:"https://google.com",
+
+capa:"https://placehold.co/300x420"
+
+});
+
+atualizarBiblioteca();
